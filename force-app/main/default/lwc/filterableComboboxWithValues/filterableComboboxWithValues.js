d1b3754cd45b7e4b1e919b0b6cdcbcd979260abf
Zsolt Harmath <zsolt.harmath@attentioncrm.com>
2025-02-05 08:26:52 +0100

    Add sample Filterable Combobox with predefined options

--- force-app/main/default/lwc/filterableComboboxWithValues/filterableComboboxWithValues.js
+++ force-app/main/default/lwc/filterableComboboxWithValues/filterableComboboxWithValues.js
@@ -5,5 +5,16 @@
 import {LightningElement} from 'lwc';
 
 export default class FilterableComboboxWithValues extends LightningElement {
+	value;
+	options = [
+		{label: 'First label', value: 'val1'},
+		{label: 'Second label', value: 'val2'},
+		{label: 'Third label', value: 'val3'},
+		{label: 'Fourth', value: 'val4'},
+	];
 
+	handleChange(event) {
+		this.value = event.detail.value;
+		console.log(`this.value`,this.value);
+	}
 }
