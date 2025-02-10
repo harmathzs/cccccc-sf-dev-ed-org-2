6e61d53c8753cff22385ca6fe8299d6ad45dbd5f
Zsolt Harmath <zsolt.harmath@attentioncrm.com>
2025-02-10 08:05:20 +0100

    Add loadRatesToEUR method to fetch exchange rates from API

--- force-app/main/default/lwc/currencyRates/currencyRates.js
+++ force-app/main/default/lwc/currencyRates/currencyRates.js
@@ -5,29 +5,50 @@
 import {LightningElement} from 'lwc';
 
 export default class CurrencyRates extends LightningElement {
-	endpoint = 'http://data.fixer.io/api/latest';
 	apiKey = '5007bbeac86574994088d4021dac9a12';
 	supportedSymbols;
 	base = 'EUR';
 	symbols;
+	ratesToEUR;
 
 	connectedCallback() {
 		this.loadSupportedSymbols();
+		this.loadRatesToEUR();
 	}
 
 	loadSupportedSymbols() {
 		fetch('https://data.fixer.io/api/symbols?access_key='+this.apiKey)
 			.then(res => res.json())
-			.then(json => this.supportedSymbols = json)
-			.catch(err => console.log(err))
-			.finally(() => {
+			.then(json => {
+				this.supportedSymbols = json;
 				console.log('loadSupportedSymbols supportedSymbols', this.supportedSymbols);
 				// {
 				//     "success": true,
 				//     "symbols": {
 				//         "AED": "United Arab Emirates Dirham",
 				//         "AFN": "Afghan Afghani",
-			});
+			})
+			.catch(err => console.log(err))
+			.finally(() => {});
+	}
+
+	loadRatesToEUR() {
+		fetch('https://data.fixer.io/api/latest?access_key='+this.apiKey+'&base=EUR')
+			.then(res => res.json())
+			.then(json => {
+				this.ratesToEUR = json;
+				console.log('loadRatesToEUR ratesToEUR', this.ratesToEUR);
+				// {
+				//     "success": true,
+				//     "timestamp": 1739168655,
+				//     "base": "EUR",
+				//     "date": "2025-02-10",
+				//     "rates": {
+				//         "AED": 3.788652,
+				//         "AFN": 76.945387,
+			})
+			.catch(err => console.log(err))
+			.finally(()=>{});
 	}
 
 	handleEndpointChange(event) {
