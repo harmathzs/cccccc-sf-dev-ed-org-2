e2b326bd436f37758e26ecd5e3483c2c2f41ad11
Zsolt Harmath <zsolt.harmath@attentioncrm.com>
2025-02-10 15:53:06 +0100

    Add chat input field with send and reset buttons

--- force-app/main/default/lwc/aiChat/aiChat.js
+++ force-app/main/default/lwc/aiChat/aiChat.js
@@ -1,9 +1,30 @@
 /**
- * Created by Harmath Zsolt on 2025. 02. 10..
+ * Created by Harmath Zsolt on 2025. 02. 10.
  */
 
 import {LightningElement} from 'lwc';
 
 export default class AiChat extends LightningElement {
+	question;
 
+	handleInputChange(event) {
+		//console.log('handleInputChange event', event);
+		this.question = event?.detail?.value;
+		console.log('handleInputChange question', this.question);
+	}
+
+	handleSendClick(event) {
+		console.log('handleSendClick event', event);
+	}
+
+	handleResetClick(event) {
+		console.log('handleResetClick event', event);
+	}
+
+	handleKeyPress(event) {
+		//console.log('handleKeyPress event', event);
+		if (event?.key == 'Enter') {
+			console.log('handleKeyPress key', event?.key);
+		}
+	}
 }
