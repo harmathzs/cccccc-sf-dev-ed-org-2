bf55dcb676346dc8e42c851b15e9a163d2789bf7
Zsolt Harmath <zsolt.harmath@attentioncrm.com>
2025-02-11 10:36:06 +0100

    feat(chat): integrate Groq API with send functionality

--- force-app/main/default/lwc/aiChat/aiChat.js
+++ force-app/main/default/lwc/aiChat/aiChat.js
@@ -3,9 +3,14 @@
  */
 
 import {LightningElement} from 'lwc';
+import calloutToGroq from '@salesforce/apex/AiChatController.calloutToGroq';
 
 export default class AiChat extends LightningElement {
 	question;
+	conversation = {
+		model: 'deepseek-r1-distill-llama-70b',
+		messages: [],
+	};
 
 	handleInputChange(event) {
 		//console.log('handleInputChange event', event);
@@ -15,6 +20,35 @@ export default class AiChat extends LightningElement {
 
 	handleSendClick(event) {
 		console.log('handleSendClick event', event);
+
+		this.conversation.messages.push({
+			role: 'user',
+			content: this.question,
+		});
+		const conversationJSON = JSON.stringify(this.conversation);
+
+		calloutToGroq({requestBodyJSON: conversationJSON})
+			.then(outputParams => {
+				console.log('calloutToGroq outputParams', outputParams);
+				const outputParamsObj = JSON.parse(outputParams);
+				//console.log('calloutToGroq outputParamsObj', JSON.stringify(outputParamsObj));
+
+				const statusCode = +outputParamsObj?.statusCode;
+				const statusText = outputParamsObj?.statusText;
+				const responseBody = outputParamsObj?.responseBody;
+				const errorMessage = outputParamsObj?.errorMessage;
+
+				console.log('calloutToGroq statusCode', statusCode);
+				console.log('calloutToGroq statusText', statusText);
+				console.log('calloutToGroq responseBody', responseBody);
+				console.log('calloutToGroq errorMessage', errorMessage);
+
+				const responseObj = JSON.parse(responseBody);
+				console.log('calloutToGroq responseObj', JSON.stringify(responseObj));
+				// TODO - merge answer into conversation
+			})
+			.catch(err => console.log(err))
+			.finally(() => {});
 	}
 
 	handleResetClick(event) {
@@ -25,6 +59,7 @@ export default class AiChat extends LightningElement {
 		//console.log('handleKeyPress event', event);
 		if (event?.key == 'Enter') {
 			console.log('handleKeyPress key', event?.key);
+			this.handleSendClick(event);
 		}
 	}
 }
